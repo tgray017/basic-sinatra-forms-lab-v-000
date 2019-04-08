8332174348f6147ce751534d6058a7335bc34d8d
Thomas Gray <tgray017@gmail.com>
2019-04-08 12:12:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,11 +9,11 @@ class App < Sinatra::Base
   post '/team' do
     @name = params[:name]
     @coach = params[:coach]
-    @point_guard = params[:point_guard]
-    @shooting_guard = params[:shooting_guard]
-    @small_forward = params[:small_forward]
-    @power_forward = params[:power_forward]
-    @center = params[:center]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     erb :team
   end
 
