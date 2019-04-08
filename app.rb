43703f78346d0bd3bcb8d464b67f03162c4a7d98
Thomas Gray <tgray017@gmail.com>
2019-04-08 12:07:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
   
   post '/team' do
-    @team_name = params[:team_name]
+    @name = params[:name]
     @coach = params[:coach]
     @point_guard = params[:point_guard]
     @shooting_guard = params[:shooting_guard]
